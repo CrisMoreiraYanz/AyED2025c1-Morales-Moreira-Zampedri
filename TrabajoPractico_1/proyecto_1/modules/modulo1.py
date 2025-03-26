89b68ed48a38e13c2b6b90520a7a79e441612fd6
NachoGalluccio <gnachoperez@gmail.com>
2025-03-26 12:39:40 -0300

    ordenamiento burbuja (no testeado)

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,19 @@
-# módulo para organizar funciones o clases utilizadas en nuestro proyecto
-# Crear tantos módulos como sea necesario para organizar el código
+#Ordenamiento Burbuja
+def bubble_sort (lista):
+    for pasar_numero in range (len(lista)-1,0,-1):
+        for i in range(pasar_numero):
+            if lista[i] > lista[i+1]:
+                temp= lista[i]
+                lista[i]=lista[i+1]
+                lista[i+1]=temp
+
+#Ordenamiento Quicksort
+def quick_sort (lista):
+    quick_sort_helper(lista,0,len(lista)-1)
+
+def quick_sort_helper(lista,primero,ultimo):
+    if primero<ultimo:
+        punto_particion=particion(lista,primero,ultimo)
+        
+
+    
