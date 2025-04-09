1019665611dbf72eabdef19930c853c260375739
unknown <CrisMoreiraYanz>
2025-04-09 12:48:16 -0300

    Agrego ordenamiento por residuos/radix sort, sin terminar

---
+++ TrabajoPractico_1/proyecto_1/modules/Ordenamiento_por_Residuos.py
@@ -0,0 +1,20 @@
+#Ordenamiento por residuos/radix sort
+def radix_sort(lista_sin_ordenar):
+    valor_maximo = max(lista_sin_ordenar)
+    exponente_maximo = len(str(valor_maximo)) 
+    ordenado = lista_sin_ordenar[:]
+    for exponente in range(max_exponent):
+        posicion = exponente + 1
+        index = - posicion
+        x = [[] for i in range(10)]
+        for numero in ordenado:
+            numero_cadena = str(numero)
+            try:
+                digito = numero_cadena[index]
+                except IndexError: 
+                digito = 0
+                digito = int(digito)
+                x[digito].append(numero)
+        ordenado = []
+        for numeral in x:
+            ordenado.extend(numeral)       
