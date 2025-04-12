2bdd6fc7d8ea69fc6e8d204013bea465aa8546c4
CrisMoreiraYanz <cmoreirayanz@gmail.com>
2025-04-12 17:43:56 -0300

    modificación del ordenamiento por residuos e incorporación de su test

--- TrabajoPractico_1/proyecto_1/modules/Ordenamiento_por_Residuos.py
+++ TrabajoPractico_1/proyecto_1/modules/Ordenamiento_por_Residuos.py
@@ -1,21 +1,14 @@
 #Ordenamiento por residuos/radix sort
-def radix_sort(lista_sin_ordenar):
-    valor_maximo = max(lista_sin_ordenar)
-    exponente_maximo = len(str(valor_maximo)) 
-    ordenado = lista_sin_ordenar[:]
-    for exponente in range(max_exponent):
-        posicion = exponente + 1
-        index = - posicion
-        x = [[] for i in range(10)]
-        for numero in ordenado:
-            numero_cadena = str(numero)
-            try:
-                digito = numero_cadena[index]
-                except IndexError: 
-                digito = 0
-                digito = int(digito)
-                x[digito].append(numero)
-        ordenado = []
-        for numeral in x:
-            ordenado.extend(numeral)       
-
+def ordenar_por_residuos(lista):
+    maximo = max(lista)
+    lugar = 1
+    while maximo // lugar > 0:
+        grupos = [[] for _ in range(10)]
+        for numero in lista:
+            digito = (numero // lugar) % 10
+            grupos[digito].append(numero)
+        lista = []
+        for grupo in grupos:
+            lista.extend(grupo)
+        lugar *= 10
+    return lista
--- TrabajoPractico_1/proyecto_1/tests/test_Residuos.py
+++ TrabajoPractico_1/proyecto_1/tests/test_Residuos.py
@@ -1,1 +1,15 @@
-# Archivo de test para realizar pruebas unitarias del modulo1
+import unittest
+from random import randint
+from modules.Ordenamiento_por_Residuos import ordenar_por_residuos
+
+class TestOrdenamientoResiduos(unittest.TestCase):
+    def setUp(self):
+        self.lista1 = [randint(10000, 99999) for _ in range(500)]
+
+    def test_lista_aleatoria_mayores_500(self):
+        self.lista_sort = sorted(self.lista1)
+        self.lista_residuos = ordenar_por_residuos(self.lista1)
+        self.assertEqual(self.lista_sort, self.lista_residuos)
+
+if __name__ == '__main__':
+    unittest.main()
