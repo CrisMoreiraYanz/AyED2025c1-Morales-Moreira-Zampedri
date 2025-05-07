e37af201c4ca4155e9f6669b3812789e4171f185
MatiasZampedri <mati.zampe2812@gmail.com>
2025-05-07 12:16:42 -0300

    arreglo cositas del monticulo

--- TrabajoPractico_2/Sala_de_emergencias/modulos/mont_binario.py
+++ TrabajoPractico_2/Sala_de_emergencias/modulos/mont_binario.py
@@ -1,6 +1,6 @@
 class monticulobinario:
     def __init__(self):
-        self.lista_monticulo = []
+        self.lista_monticulo = [None]
         self.tamanio = 0
         
     def infiltrar_arriba(self,valor):
@@ -10,10 +10,11 @@ class monticulobinario:
             self.lista_monticulo[valor//2]=self.lista_monticulo[valor]
             self.lista_monticulo[valor]=tmp
           valor=valor//2
+
     def insertar(self,valor):
-       self.lista_monticulo.append(valor)
-       self.tamanio+=1
-       self.infiltrar_arriba(self.tamanio)     
+      self.lista_monticulo.append(valor)
+      self.tamanio+=1
+      self.infiltrar_arriba(self.tamanio)     
 
     def hijo_min(self, valor):
         if valor * 2 + 1 > self.tamanio:
@@ -25,13 +26,13 @@ class monticulobinario:
              return valor * 2 + 1
         
     def infiltrar_abajo(self,valor):
-       while (valor * 2) <= self.tamanio:
-           hm = self.hijo_min(valor)
-           if self.lista_monticulo[valor] > self.lista_monticulo[hm]:
+      while (valor * 2) <= self.tamanio:
+         hm = self.hijo_min(valor)
+         if self.lista_monticulo[valor] > self.lista_monticulo[hm]:
                tmp = self.lista_monticulo[valor]
                self.lista_monticulo[valor] = self.lista_monticulo[hm]
                self.lista_monticulo[hm] = tmp
-       valor = hm      
+         valor = hm      
 
     def extraer_minimo(self):
        ret_val = self.lista_monticulo[1]
@@ -48,16 +49,22 @@ class monticulobinario:
        while (i > 0):
          self.infiltrar_abajo(i)
          i -= 1   
+        
 
-if __name__=="__main__":
-    monticulo = monticulobinario()
-    lista = [5, 3, 8, 1, 4, 7, 2, 6]
-    monticulo.construir_monticulo(lista)
-    print("Montículo binario construido:", monticulo.lista_monticulo[1:])
+if __name__== "__main__":
+   monticulo = monticulobinario()
+   lista = [5, 3, 8, 1, 4, 7, 2, 6]
+   monticulo.construir_monticulo(lista)
+   # monticulo.insertar(5)
+   # monticulo.insertar(3)
+   # monticulo.insertar(8)
+   print("Montículo binario construido:", monticulo.lista_monticulo[1:])
     
-    monticulo.insertar(0)
-    print("Después de insertar 0:", monticulo.lista_monticulo[1:])
+   monticulo.insertar(0)
+   print("Después de insertar 0:", monticulo.lista_monticulo[1:])
     
-    min_val = monticulo.extraer_minimo()
-    print("Valor mínimo extraído:", min_val)
-    print("Después de extraer el mínimo:", monticulo.lista_monticulo[1:])
+   min_val = monticulo.extraer_minimo()
+   print("Valor mínimo extraído:", min_val)
+   print("Después de extraer el mínimo:", monticulo.lista_monticulo[1:])
+
+   print ("Fin del programa")
